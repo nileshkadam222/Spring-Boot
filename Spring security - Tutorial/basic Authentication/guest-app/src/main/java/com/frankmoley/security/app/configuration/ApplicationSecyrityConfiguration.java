378d1eb9ac0c6d0aa27893decbdab167f25b83ee
Nilesh <nilesh.kadam222@gmail.com>
2020-01-12 21:22:53 +0530

    InMempryUserDetailsManager usages i.e In memory Authentication

--- Spring security - Tutorial/basic Authentication/guest-app/src/main/java/com/frankmoley/security/app/configuration/ApplicationSecyrityConfiguration.java
+++ Spring security - Tutorial/basic Authentication/guest-app/src/main/java/com/frankmoley/security/app/configuration/ApplicationSecyrityConfiguration.java
@@ -1,9 +1,17 @@
 package com.frankmoley.security.app.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +25,14 @@ public class ApplicationSecyrityConfiguration extends WebSecurityConfigurerAdapt
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<UserDetails>();
+        userDetailsList.add(User.withDefaultPasswordEncoder().username("NileshK").password("12345").roles("USER","ADMIN").build());
+        userDetailsList.add(User.withDefaultPasswordEncoder().username("Nilesh").password("12345").roles("USER","ADMIN").build());
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
