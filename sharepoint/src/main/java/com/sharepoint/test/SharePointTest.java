c5061ed2159a753b8aaed976c7e50a5e4a43afbd
Nilesh kadam <nilesh.kadam222@gmail.com>
2021-03-26 11:44:31 +0530

    check folder exist method

--- sharepoint/src/main/java/com/sharepoint/test/SharePointTest.java
+++ sharepoint/src/main/java/com/sharepoint/test/SharePointTest.java
@@ -9,18 +9,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.common.base.Predicate;
+import com.sharepoint.rest.PLGSharepointClient;
 import com.sharepoint.rest.PLGSharepointClientOnline;
 
 public class SharePointTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SharePointTest.class);
+
 	public static void main(String[] args) throws Exception {
 		/*
 		 * domain: nileshkadam.sharepoint.com passwd: Debugger@111 spSieUrl: /sites/MRM
@@ -35,45 +33,68 @@ public class SharePointTest {
 		String spSiteUrl = "/sites/MRM";
 
 		// get folder List
-		PLGSharepointClientOnline wrapper = new PLGSharepointClientOnline(user, passwd, domain, spSiteUrl);
-		/*
-		 * try { JSONObject result =
-		 * wrapper.getFolderByRelativeUrl("/sites/MRM/Shared Documents/MRM", "{}");
-		 * System.out.println(result); } catch (Exception e) { e.printStackTrace(); }
-		 */
+		PLGSharepointClient wrapper = new PLGSharepointClientOnline(user, passwd, domain, spSiteUrl);
+		try {
+			JSONObject result = wrapper.getFolderByRelativeUrl("/sites/MRM/Shared Documents/MRM", "{}");
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		/*
-		 * // Create New Folder // PLGSharepointClient wrapper = new
-		 * PLGSharepointClient(user, passwd, domain, // spSiteUrl); try {
-		 * 
-		 * JSONObject payload = new JSONObject(); //
-		 * payload.put("Title","Document Title set with the API"); Date date = new
-		 * Date(); Timestamp folderName = new Timestamp(date.getTime()); JSONObject
-		 * result = wrapper.createFolder("/sites/MRM/Shared Documents/MRM",
-		 * "test"+folderName.getTime(), payload); System.out.println(result); } catch
-		 * (Exception e) { e.printStackTrace(); }
-		 * 
-		 * // Upload File String fileName =""; try { Date date = new Date(); Timestamp
-		 * timeStamp = new Timestamp(date.getTime()); fileName =
-		 * timeStamp.getTime()+".txt"; MultipartFile multipartFile = new
-		 * MockMultipartFile(fileName, "Hello World".getBytes()); // payload is a json
-		 * object where to place metadata properties to associate with // file in this
-		 * example i set Title JSONObject payload = new JSONObject(); //
-		 * payload.put("Title","Document Title set with the API");
-		 * 
-		 * JSONObject result = wrapper.uploadFile("/sites/MRM/Shared Documents/MRM",
-		 * multipartFile.getResource(), fileName, payload); System.out.println(result);
-		 * } catch (Exception e) { e.printStackTrace(); }
-		 * 
-		 * 
-		 * //Download file try { JSONObject payload = new JSONObject(); Resource result
-		 * = wrapper.downloadFile("/sites/MRM/Shared Documents/MRM/"+fileName);
-		 * InputStream ins = result.getInputStream(); try(InputStreamReader r = new
-		 * InputStreamReader(ins)){ int data = r.read(); while (data != -1) {
-		 * System.out.print((char) data); data = r.read(); } }catch (Exception e) {
-		 * e.printStackTrace(); // TODO: handle exception } System.out.println(result);
-		 * } catch (Exception e) { e.printStackTrace(); }
-		 */
+		// Create New Folder
+		// PLGSharepointClient wrapper = new PLGSharepointClient(user, passwd, domain,
+		// spSiteUrl);
+		try {
+			
+			JSONObject payload = new JSONObject();
+			// payload.put("Title","Document Title set with the API");
+			Date date = new Date();
+			Timestamp folderName = new Timestamp(date.getTime());
+			JSONObject result = wrapper.createFolder("/sites/MRM/Shared Documents/MRM", "test"+folderName.getTime(), payload);
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Upload File
+		String fileName ="";
+		try {
+			Date date = new Date();
+			Timestamp timeStamp = new Timestamp(date.getTime());
+			fileName = timeStamp.getTime()+".txt";
+			MultipartFile multipartFile = new MockMultipartFile(fileName, "Hello World".getBytes());
+			// payload is a json object where to place metadata properties to associate with
+			// file in this example i set Title
+			JSONObject payload = new JSONObject();
+			// payload.put("Title","Document Title set with the API");
+
+			JSONObject result = wrapper.uploadFile("/sites/MRM/Shared Documents/MRM", multipartFile.getResource(),
+					fileName, payload);
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		//Download file
+		try {
+			JSONObject payload = new JSONObject();
+			Resource result = wrapper.downloadFile("/sites/MRM/Shared Documents/MRM/"+fileName);
+			InputStream ins = result.getInputStream();
+			try(InputStreamReader r = new InputStreamReader(ins)){
+				int data = r.read();
+			    while (data != -1) {
+			        System.out.print((char) data);
+			        data = r.read();
+			    }
+			}catch (Exception e) {
+				e.printStackTrace();
+				// TODO: handle exception
+			}
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		//get folders list
 		try {
