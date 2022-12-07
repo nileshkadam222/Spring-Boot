5038e8f00f26d3a41270abf005144e985dbba7c2
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2022-12-07 20:33:58 +0530

    Property injection i.e field injection

--- spring-demo-one/src/com/luv2code/springdemo/CrecketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CrecketCoach.java
@@ -4,6 +4,21 @@ public class CrecketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	private String emailAddress;
+	
+	private String teamName;
+	
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Inside setting emailAddress method. "+emailAddress);
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeamName(String teamName) {
+		System.out.println("Inside setting team method. " + teamName);
+		this.teamName = teamName;
+	}
+
 	public CrecketCoach() {
 		System.out.println("Inside No-Args constructor.");
 	}
