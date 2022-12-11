8b101c82dcf811cff41c91c24165e45daff7dcdd
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2022-12-11 18:05:45 +0530

    Bean Scope @scope annotation

---
+++ spring-demo-annotation/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
+		
+		Coach alphaCoach = context.getBean("tennisCoach",Coach.class);
+		
+		boolean result = (theCoach == alphaCoach);
+		
+		System.out.println("poinitng to the same object: "+result);
+		
+		System.out.println("Memory locaation for theCoach: "+theCoach);
+		
+		System.out.println("Memory locaation for alphaCoach: "+alphaCoach);
+		
+		context.close();
+	}
+
+}
