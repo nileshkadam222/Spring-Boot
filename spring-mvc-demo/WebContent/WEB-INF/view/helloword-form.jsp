cce26ba00a269762358a1417e897daadfaeba4df
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2023-01-18 19:35:43 +0530

    Future in Aysnc

--- spring-mvc-demo/WebContent/WEB-INF/view/helloword-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloword-form.jsp
@@ -3,7 +3,7 @@
 	<title>Input form</title>
 </head>
 <body>
-	<form action="GET">
+	<form action="/spring-mvc-demo/welcome" method="post">
 		<input type="text" placeholder="What is your name?" name="studentName">
 		<input type="submit"/> 
 	</form>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWordController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWordController.java
@@ -1,7 +1,10 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWordController {
@@ -9,6 +12,11 @@ public class HelloWordController {
 	@RequestMapping("/showform")
 	public String showForm() {
 		return "helloword-form";
+	}
+	
+	@RequestMapping(value = "/welcome", method = RequestMethod.POST)
+	public String showWelcome(ModelAttribute modelAttribute) {
+		return "welcome";
 	}
 
 }
