03c12e44a16320157d7ee86fbf265f27e57218b0
Nilesh Ashok kadam <47922340+nileshkadam222@users.noreply.github.com>
2025-11-23 19:53:07 +0530

    improve code

--- jwt/src/main/java/com/nilhartech/jwt/security/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/nilhartech/jwt/security/JwtAuthenticationFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,9 +29,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal( @NonNull HttpServletRequest request,
+                                     @NonNull HttpServletResponse response,
+                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         // get JWT (token) from http request
         String token = getJWTfromRequest(request);
         // validate token
@@ -55,7 +55,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String getJWTfromRequest(HttpServletRequest request){
             String bearerToken = request.getHeader("Authorization");
             if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
-                return bearerToken.substring(7, bearerToken.length());
+                return bearerToken.substring(7);
             }
             return null;
     }
